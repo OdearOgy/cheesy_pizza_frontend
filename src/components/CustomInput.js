08b21b1af441f050ae37c600df4a1b1006edc6d9
OdearOgy <hovhan198@gmail.com>
2020-05-28 01:00:28 +0400

    Fix custom input component

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useField } from 'formik';
 
 const CustomInput = (props) => {
-	const { label, labelstyles, errorstyles, InputComponent = 'input', inputstyles } = props;
+	const { label, labelstyles, errorstyles, inputcomponent = 'input', inputstyles } = props;
 	const [field, meta] = useField(props);
-
+	const Input = inputcomponent;
 	return (
 		<label className={labelstyles}>
 			<div>{label}</div>
-			<InputComponent {...props} {...field} className={inputstyles} />
+			<Input {...props} {...field} className={inputstyles} />
 			{meta.touched && meta.error ? <div className={errorstyles}>{meta.error}</div> : null}
 		</label>
 	);
